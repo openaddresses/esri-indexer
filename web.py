18f4c6be4406f1e114a59bee00d67c34de8de39a
Ian Dees <ian.dees@gmail.com>
2015-03-29 18:46:59 -0400

    Add Flask-Sqlalchemy + Heroku postgres

--- web.py
+++ web.py
@@ -1,7 +1,10 @@
 import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('HEROKU_POSTGRESQL_VIOLET_URL')
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
